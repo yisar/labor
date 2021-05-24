75ae2cd70e6c599cb17fbc94a28f28057c6745be
132yse <1533540012@qq.com>
2021-05-24 20:54:25 +0800

    add utils

---
+++ sw.js
@@ -0,0 +1,36 @@
+importScripts('https://cdn.jsdelivr.net/gh/golang/go@go1.15.7/misc/wasm/wasm_exec.js')
+
+function registerLaborListener(wasm, { base, args = [] } = {}) {
+  let path = new URL(registration.scope).pathname
+  if (base && base !== '') path = `${trimEnd(path, '/')}/${trimStart(base, '/')}`
+
+  const handlerPromise = new Promise(setHandler => {
+    self.labor = {
+      path,
+      setHandler,
+    }
+  })
+
+  const go = new Go()
+  go.argv = [wasm, ...args]
+  WebAssembly.instantiateStreaming(fetch(wasm), go.importObject).then(({ instance }) => go.run(instance))
+
+  addEventListener('fetch', e => {
+    const { pathname } = new URL(e.request.url)
+    if (!pathname.startsWith(path)) return
+
+    e.respondWith(handlerPromise.then(handler => handler(e.request)))
+  })
+}
+
+function trimStart(s, c) {
+  let r = s
+  while (r.startsWith(c)) r = r.slice(c.length)
+  return r
+}
+
+function trimEnd(s, c) {
+  let r = s
+  while (r.endsWith(c)) r = r.slice(0, -c.length)
+  return r
+}
